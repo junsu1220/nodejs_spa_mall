f455d27b8dbdc324310fb1775dc6c9b44bc09251
junsu1220 <crab7157@gmail.com>
2022-08-04 15:32:45 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const connect = require("./schemas");
+connect();
+
+// app.js
+const goodsRouter = require("./routes/goods");
+
+app.use(express.json());
+
+app.use("/api", [goodsRouter]);
+
+// app.use("/api", [goodsRouter]);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 열렸어요!");
+});
